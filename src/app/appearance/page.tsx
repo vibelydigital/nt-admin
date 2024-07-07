366ce31c903741c88ef90ef8380c0ea24ae9be77
imran <developerimranhossen@gmail.com>
2024-07-07 13:01:44 +0600

    Initial commit: Git clone frome vibly-digital/nt-admin Install frome TailAdmin Status - done

---
+++ src/app/appearance/page.tsx
@@ -0,0 +1,12 @@
+import DefaultLayout from '@/components/Layouts/DefaultLayout';
+import React from 'react'
+
+function Appearance() {
+    return (
+        <DefaultLayout>
+          Appearance page.
+        </DefaultLayout>
+      );
+}
+
+export default Appearance
---
+++ src/app/plugins/page.tsx
@@ -0,0 +1,13 @@
+import DefaultLayout from '@/components/Layouts/DefaultLayout'
+import React from 'react'
+
+function PluginPage() {
+  return (
+    <DefaultLayout>
+        <div>PluginPage</div>
+    </DefaultLayout>
+    
+  )
+}
+
+export default PluginPage
