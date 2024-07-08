49149c0580c8154e2b8323cb5fe7a920d5c2c2bc
imran <developerimranhossen@gmail.com>
2024-07-08 10:51:41 +0600

    add 'pages' named page

---
+++ src/app/pages/page.tsx
@@ -0,0 +1,13 @@
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+import ListBuilder from "@/components/Tables/ListBuilder"
+
+const AllPages = () => {
+
+    return (
+      <DefaultLayout>
+        <ListBuilder />
+      </DefaultLayout>
+    );
+  };
+  
+  export default AllPages;
--- src/components/Tables/ListBuilder.tsx
+++ src/components/Tables/ListBuilder.tsx
@@ -13,7 +13,7 @@ const brandData: BRAND[] = [
 ];
 
 const TableBuilder = () => {
-  console.log(brandData[0].date);
+ 
   return (
     <div className="rounded-sm border border-stroke bg-white px-5 pb-2.5 pt-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
       <h4 className="mb-6 text-xl font-semibold text-black dark:text-white">
