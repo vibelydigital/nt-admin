c1f48ecc118a7082c02a22a5698f207a0d19bc93
imran <developerimranhossen@gmail.com>
2024-07-15 14:36:55 +0600

    feat: Implement AddCategoryModal component

--- src/components/Posts/Categories.tsx
+++ src/components/Posts/Categories.tsx
@@ -2,273 +2,35 @@
 import { useState } from "react";
 import CategoryList from "./CategoryList";
 import AddCategory from "./AddCategory";
+import AddCategoryModal from "./AddCategoryModal";
 
 function Categories() {
-  const defaultCategory = [
-    {
-      id: crypto.randomUUID(),
-      title: "Learn React Native",
-      description:
-        "I want to Learn React such thanI can treat it like my slave and make it do whatever I want to do.",
-      tags: ["web", "react", "js"],
-      priority: "High",
-      isFavorite: true,
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Learn React Native",
-      description:
-        "I want to Learn React such thanI can treat it like my slave and make it do whatever I want to do.",
-      tags: ["web", "react", "js"],
-      priority: "High",
-      isFavorite: true,
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Learn React Native",
-      description:
-        "I want to Learn React such thanI can treat it like my slave and make it do whatever I want to do.",
-      tags: ["web", "react", "js"],
-      priority: "High",
-      isFavorite: true,
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Learn React Native",
-      description:
-        "I want to Learn React such thanI can treat it like my slave and make it do whatever I want to do.",
-      tags: ["web", "react", "js"],
-      priority: "High",
-      isFavorite: true,
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Learn React Native",
-      description:
-        "I want to Learn React such thanI can treat it like my slave and make it do whatever I want to do.",
-      tags: ["web", "react", "js"],
-      priority: "High",
-      isFavorite: true,
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Learn React Native",
-      description:
-        "I want to Learn React such thanI can treat it like my slave and make it do whatever I want to do.",
-      tags: ["web", "react", "js"],
-      priority: "High",
-      isFavorite: true,
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Learn React Native",
-      description:
-        "I want to Learn React such thanI can treat it like my slave and make it do whatever I want to do.",
-      tags: ["web", "react", "js"],
-      priority: "High",
-      isFavorite: true,
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Learn React Native",
-      description:
-        "I want to Learn React such thanI can treat it like my slave and make it do whatever I want to do.",
-      tags: ["web", "react", "js"],
-      priority: "High",
-      isFavorite: true,
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Learn React Native",
-      description:
-        "I want to Learn React such thanI can treat it like my slave and make it do whatever I want to do.",
-      tags: ["web", "react", "js"],
-      priority: "High",
-      isFavorite: true,
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Learn React Native",
-      description:
-        "I want to Learn React such thanI can treat it like my slave and make it do whatever I want to do.",
-      tags: ["web", "react", "js"],
-      priority: "High",
-      isFavorite: true,
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Learn React Native",
-      description:
-        "I want to Learn React such thanI can treat it like my slave and make it do whatever I want to do.",
-      tags: ["web", "react", "js"],
-      priority: "High",
-      isFavorite: true,
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Learn React Native",
-      description:
-        "I want to Learn React such thanI can treat it like my slave and make it do whatever I want to do.",
-      tags: ["web", "react", "js"],
-      priority: "High",
-      isFavorite: true,
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Learn React Native",
-      description:
-        "I want to Learn React such thanI can treat it like my slave and make it do whatever I want to do.",
-      tags: ["web", "react", "js"],
-      priority: "High",
-      isFavorite: true,
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Learn React Native",
-      description:
-        "I want to Learn React such thanI can treat it like my slave and make it do whatever I want to do.",
-      tags: ["web", "react", "js"],
-      priority: "High",
-      isFavorite: true,
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Learn React Native",
-      description:
-        "I want to Learn React such thanI can treat it like my slave and make it do whatever I want to do.",
-      tags: ["web", "react", "js"],
-      priority: "High",
-      isFavorite: true,
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Learn React Native",
-      description:
-        "I want to Learn React such thanI can treat it like my slave and make it do whatever I want to do.",
-      tags: ["web", "react", "js"],
-      priority: "High",
-      isFavorite: true,
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Learn React Native",
-      description:
-        "I want to Learn React such thanI can treat it like my slave and make it do whatever I want to do.",
-      tags: ["web", "react", "js"],
-      priority: "High",
-      isFavorite: true,
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Learn React Native",
-      description:
-        "I want to Learn React such thanI can treat it like my slave and make it do whatever I want to do.",
-      tags: ["web", "react", "js"],
-      priority: "High",
-      isFavorite: true,
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Learn React Native",
-      description:
-        "I want to Learn React such thanI can treat it like my slave and make it do whatever I want to do.",
-      tags: ["web", "react", "js"],
-      priority: "High",
-      isFavorite: true,
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Learn React Native",
-      description:
-        "I want to Learn React such thanI can treat it like my slave and make it do whatever I want to do.",
-      tags: ["web", "react", "js"],
-      priority: "High",
-      isFavorite: true,
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Learn React Native",
-      description:
-        "I want to Learn React such thanI can treat it like my slave and make it do whatever I want to do.",
-      tags: ["web", "react", "js"],
-      priority: "High",
-      isFavorite: true,
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Learn React Native",
-      description:
-        "I want to Learn React such thanI can treat it like my slave and make it do whatever I want to do.",
-      tags: ["web", "react", "js"],
-      priority: "High",
-      isFavorite: true,
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Learn React Native",
-      description:
-        "I want to Learn React such thanI can treat it like my slave and make it do whatever I want to do.",
-      tags: ["web", "react", "js"],
-      priority: "High",
-      isFavorite: true,
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Learn React Native",
-      description:
-        "I want to Learn React such thanI can treat it like my slave and make it do whatever I want to do.",
-      tags: ["web", "react", "js"],
-      priority: "High",
-      isFavorite: true,
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Learn React Native",
-      description:
-        "I want to Learn React such thanI can treat it like my slave and make it do whatever I want to do.",
-      tags: ["web", "react", "js"],
-      priority: "High",
-      isFavorite: true,
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Learn React Native",
-      description:
-        "I want to Learn React such thanI can treat it like my slave and make it do whatever I want to do.",
-      tags: ["web", "react", "js"],
-      priority: "High",
-      isFavorite: true,
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Learn React Native",
-      description:
-        "I want to Learn React such thanI can treat it like my slave and make it do whatever I want to do.",
-      tags: ["web", "react", "js"],
-      priority: "High",
-      isFavorite: true,
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Learn React Native",
-      description:
-        "I want to Learn React such thanI can treat it like my slave and make it do whatever I want to do.",
-      tags: ["web", "react", "js"],
-      priority: "High",
-      isFavorite: true,
-    },
-  ];
+  const defaultCategory = {
+    id: crypto.randomUUID(),
+    title: "Learn React Native",
+    description:
+      "I want to Learn React such thanI can treat it like my slave and make it do whatever I want to do.",
+    tags: ["web", "react", "js"],
+    priority: "High",
+    isFavorite: true,
+  };
 
-  const [categories, setcategories] = useState(defaultCategory);
+  const [categories, setcategories] = useState([defaultCategory]);
+  const [showAddModal, setShowAddModal] = useState(false);
   function handleDeleteCategory(categoryId: string) {
     const categoriesAfterDelete = categories.filter(
       (categorie) => categorie.id !== categoryId,
     );
     setcategories(categoriesAfterDelete);
   }
+
+  function handleAddCategory() {
+    console.log("added");
+  }
   return (
     <div className="w-1/2 overflow-auto">
-      <AddCategory />
+      {showAddModal && (<AddCategoryModal />)}
+      <AddCategory onHandle={() => setShowAddModal(true)} />
 
       <table className="table-fixed overflow-auto xl:w-full">
         <thead>
